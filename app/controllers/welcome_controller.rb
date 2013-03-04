3de2cc1d154c88a5e16f3e28dd036726f796740a
David Henner <drhenner@yahoo.com>
2013-03-04 01:22:28 -0800

    welcome page changes if we aren't in the signup period

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
   skip_before_filter :redirect_to_welcome
-  layout 'welcome'
+  layout 'application'
 
   def index
     @user = User.new
+    render :template => 'welcome/signup', :layout => 'welcome' if Settings.in_signup_period
   end
 end
