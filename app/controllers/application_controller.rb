fa2637f6ac1dc4794bcd68331f1c710677f4f98a
David Henner <drhenner@yahoo.com>
2013-03-05 10:56:28 -0800

    add password to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  http_basic_authenticate_with :name => "ror-e", :password => "ufcfit"
   layout 'application'
 
   helper_method :current_user,
