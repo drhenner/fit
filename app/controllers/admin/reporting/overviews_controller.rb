aa8605b05cf85e1aa88320a68e76186acae31ac1
David Henner <drhenner@yahoo.com>
2013-03-05 23:09:28 -0800

    reporting

--- app/controllers/admin/reporting/overviews_controller.rb
+++ app/controllers/admin/reporting/overviews_controller.rb
@@ -1,11 +1,29 @@
-#require 'reports/accounting.rb'
 class Admin::Reporting::OverviewsController < Admin::Reporting::BaseController
+  before_filter :set_time_range
 
   def show
-    start_time, end_time = ROReReports::Accounting.weekly
-    @accounting_report = ROReReports::Accounting.new(start_time, end_time)
+    params[:start_time] if params[:start_time].present?
+    @accounting_report  = ROReReports::Accounting.new(start_time, end_time)
+    @orders_report      = ROReReports::Orders.new(start_time, end_time)
+    @customers_report   = ROReReports::Customers.new(start_time, end_time)
   end
 
   private
 
+  def set_time_range
+    if params[:start_time].present?
+      @start_time = Time.parse(params[:start_time])
+      @end_time   = params[:start_time].present? ? Time.parse(params[:end_time]) : Time.zone.now
+    else
+      @start_time, @end_time = ROReReports::Accounting.weekly
+    end
+  end
+
+  def start_time
+    @start_time
+  end
+
+  def end_time
+    @end_time
+  end
 end
--- config/initializers/libs.rb
+++ config/initializers/libs.rb
@@ -1,2 +1,4 @@
 require 'custom_validators'
 require 'reports/accounting'
+require 'reports/orders'
+require 'reports/customers'
