95e24430334d682c3f6c0605aa492a2fbba0654d
David Henner <drhenner@yahoo.com>
2013-03-08 17:25:48 -0800

    air brake only in production

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,5 @@
-Airbrake.configure do |config|
-  config.api_key = '75044c76463c0f5c11f163627e5ce0da'
+if Rails.env == 'production'
+  Airbrake.configure do |config|
+    config.api_key = '75044c76463c0f5c11f163627e5ce0da'
+  end
 end
