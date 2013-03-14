236ffc916a3fe4fc35effee6af44518fa8414fb0
David Henner <drhenner@yahoo.com>
2013-03-14 15:06:46 -0700

    unsubscribe link has no layout...

--- app/controllers/unsubscribes_controller.rb
+++ app/controllers/unsubscribes_controller.rb
@@ -2,7 +2,7 @@ class UnsubscribesController < ApplicationController
   skip_before_filter :redirect_to_welcome
   def show
     UsersNewsletter.unsubscribe(params[:email], params[:key])
-
+    render :layout => 'blank'
   end
 
 end
