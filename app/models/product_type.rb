b5994ff7c3c45f71089023f9614866545d9a8c55
David Henner <drhenner@yahoo.com>
2013-03-20 21:07:37 -0700

    for now these are the upsells on the page

--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -12,8 +12,14 @@ class ProductType < ActiveRecord::Base
   # @return [ Array[ProductType] ]
   def self.admin_grid(params = {})
     grid = ProductType
-    grid = grid.where("product_types.name LIKE ?", "#{params[:name]}%")              if params[:name].present?
+    grid = grid.where("product_types.name iLIKE ?", "#{params[:name]}%")              if params[:name].present?
     grid
+  end
+
+  def self.upsell_product_type_ids
+    Rails.cache.fetch("upsell_product_type_ids", :expires_in => 3.hours) do
+      ProductType.where("product_types.name NOT iLIKE ?", 'Media').pluck(:id)
+    end
   end
 
 end
