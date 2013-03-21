14d64c5072816f99b43ed1d969254bc384e71e43
David Henner <drhenner@yahoo.com>
2013-03-21 00:24:40 -0700

    upsell controller

---
+++ spec/controllers/upsells_controller_spec.rb
@@ -0,0 +1,24 @@
+require  'spec_helper'
+
+describe UpsellsController do
+  # fixtures :all
+  render_views
+
+  before do
+    @cart      = FactoryGirl.create(:cart)
+    @cart_item = FactoryGirl.create(:cart_item, :cart => @cart)
+    @controller.stubs(:session_cart).returns(@cart)
+  end
+
+  it "update action should redirect when model is valid" do
+    variant = FactoryGirl.create(:variant)
+    put :update, :id => variant.id
+    expect(response).to redirect_to(preorders_url)
+  end
+
+  it "destroy action should destroy model and redirect to index action" do
+    delete :destroy, :id => @cart_item.id
+    expect(response).to redirect_to(preorders_url)
+    CartItem.find(@cart_item.id).active.should be_false
+  end
+end
