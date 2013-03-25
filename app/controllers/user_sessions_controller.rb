bcbfa7bd81f133e6212b6e742fe827ee702b322a
David Henner <drhenner@yahoo.com>
2013-03-25 16:33:59 -0700

    remove commented out code

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -15,7 +15,6 @@ class UserSessionsController < ApplicationController
       set_user_to_cart_items
       flash[:notice] = I18n.t('login_successful')
       @user = User.new
-      #render :template => '/welcome/index', :layout => 'welcome'
       if @user_session.record.admin?
         redirect_to admin_users_url
       else
