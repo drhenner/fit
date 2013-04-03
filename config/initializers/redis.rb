b040d72f41e2cda9313652ae88879c546c6f6884
David Henner <drhenner@yahoo.com>
2013-04-03 16:33:27 -0700

    get redis working on heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,4 @@
-$redis = Redis.connect(url: (YAML.load(ERB.new(Rails.root.join("config", "redis.yml").read).result) rescue {})[Rails.env])
+# $redis = Redis.connect(url: (YAML.load(ERB.new(Rails.root.join("config", "redis.yml").read).result) rescue {})[Rails.env])
+
+uri = URI.parse((YAML.load(ERB.new(Rails.root.join("config", "redis.yml").read).result) rescue {})[Rails.env])
+Resque.redis = REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
