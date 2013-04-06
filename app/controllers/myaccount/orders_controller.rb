f069d6da19fc103885b4d7623d823a079f1b49fb
David Henner <drhenner@yahoo.com>
2013-04-05 17:47:47 -0700

    N+1 query

--- app/controllers/myaccount/orders_controller.rb
+++ app/controllers/myaccount/orders_controller.rb
@@ -8,7 +8,9 @@ class Myaccount::OrdersController < Myaccount::BaseController
   # GET /myaccount/orders/1
   # GET /myaccount/orders/1.xml
   def show
-    @order = current_user.viewable_orders.includes([:invoices]).find_by_number(params[:id])
+    @order = current_user.viewable_orders.
+                          includes([:invoices, :ship_address, :bill_address, {:order_items => {:variant => :product}}]).
+                          find_by_number(params[:id])
   end
   private
 
