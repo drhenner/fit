0a1af471f9f538c877cf56126e1893247bfef2d7
David Henner <drhenner@yahoo.com>
2013-04-06 01:50:28 -0700

    require old password to change any details

--- app/controllers/myaccount/overviews_controller.rb
+++ app/controllers/myaccount/overviews_controller.rb
@@ -9,9 +9,15 @@ class Myaccount::OverviewsController < Myaccount::BaseController
 
   def update
     @user = current_user
-    if @user.update_attributes(params[:user])
-      redirect_to myaccount_overview_url(), :notice  => "Successfully updated user."
+
+    if @user.valid_password?(params[:old_password])
+      if @user.update_attributes(params[:user])
+        redirect_to myaccount_overview_url(), :notice  => "Successfully updated user."
+      else
+        render :edit
+      end
     else
+      flash[:alert] = 'Your old Password is not correct.'
       render :edit
     end
   end
