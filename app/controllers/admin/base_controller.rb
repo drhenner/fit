1b078e69a957bd93490aa23231fc123ea49dee1d
David Henner <drhenner@yahoo.com>
2013-04-07 22:27:13 -0700

    hold 12 of the most recent

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -20,7 +20,7 @@ class Admin::BaseController < ApplicationController
     session[:recent_users] ||= []
     if session[:recent_users].any?{|email, id| id == user.id }
       session[:recent_users].delete_if {|email, id| id == user.id}
-    elsif session[:recent_users].size > 10
+    elsif session[:recent_users].size > 12
       session[:recent_users].pop
     end
     session[:recent_users].unshift( [user.email, user.id] )
