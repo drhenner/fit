179641bac286a6756f1b9a8620a5bd94cbab24cf
Khurram Zaman <khurram.zaman@gmail.com>
2013-04-10 23:13:12 +0500

    Schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130410013718) do
+ActiveRecord::Schema.define(:version => 20130410173334) do
 
   create_table "accounting_adjustments", :force => true do |t|
     t.integer  "adjustable_id",                                 :null => false
@@ -244,8 +244,9 @@ ActiveRecord::Schema.define(:version => 20130410013718) do
   end
 
   create_table "newsletters", :force => true do |t|
-    t.string  "name",          :null => false
-    t.boolean "autosubscribe", :null => false
+    t.string  "name",              :null => false
+    t.boolean "autosubscribe",     :null => false
+    t.string  "mailchimp_list_id"
   end
 
   create_table "order_items", :force => true do |t|
