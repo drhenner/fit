848fd985b3058fa5f31f193e305b62c35c781b84
David Henner <drhenner@yahoo.com>
2013-04-10 17:39:23 -0700

    order_id might be bad for a number of reasons (mostly for developers)

--- app/controllers/shopping/base_controller.rb
+++ app/controllers/shopping/base_controller.rb
@@ -77,8 +77,8 @@ class Shopping::BaseController < ApplicationController
   def find_or_create_order
     return @session_order if @session_order
     if session[:order_id]
-      @session_order = current_user.orders.include_checkout_objects.find(session[:order_id])
-      create_order if !@session_order.in_progress?
+      @session_order = current_user.orders.include_checkout_objects.find_by_id(session[:order_id])
+      create_order if !@session_order || !@session_order.in_progress?
     else
       create_order
     end
