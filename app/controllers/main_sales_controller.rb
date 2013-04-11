ee09eb9b6db3a46ee9aa7287ad980519e7cf99bb
David Henner <drhenner@yahoo.com>
2013-04-11 02:43:46 -0700

    much better navigation

--- app/controllers/main_sales_controller.rb
+++ app/controllers/main_sales_controller.rb
@@ -3,7 +3,7 @@ class MainSalesController < ApplicationController
   def update
     @variant = Variant.find(params[:variant_id])
     session_cart.change_main_sale(@variant, most_likely_user)
-    redirect_to preorders_url
+    redirect_to preorders_url(:anchor => 'your-cart-items')
   end
 
 end
