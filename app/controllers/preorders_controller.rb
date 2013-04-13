692fa278f69c419bb4e7a41cf10e312436f8882c
David Henner <drhenner@yahoo.com>
2013-04-13 04:20:31 -0700

    don't skip before filter

--- app/controllers/preorders_controller.rb
+++ app/controllers/preorders_controller.rb
@@ -1,6 +1,5 @@
 class PreordersController < ApplicationController
   helper_method :upsells
-  #skip_before_filter :redirect_to_welcome
   before_filter :redirect_unless_preorder
   layout 'preorder'
 
