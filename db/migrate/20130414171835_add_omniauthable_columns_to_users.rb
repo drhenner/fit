9058afabe86cec469898c44f5ce1d9fc7091d66e
Khurram Zaman <khurram.zaman@gmail.com>
2013-04-14 22:24:51 +0500

    OmniAuth columns added in Users

---
+++ db/migrate/20130414171835_add_omniauthable_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddOmniauthableColumnsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_index :users, :uid, unique: true
+  end
+end
