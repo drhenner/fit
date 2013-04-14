98f91b2a702047c60430107d16a78ef8c0cbb5e3
Khurram Zaman <khurram.zaman@gmail.com>
2013-04-14 22:24:16 +0500

    Omniauth gem, strategy and initializer added.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+require 'omniauth-ufc'
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :ufc, Settings.ufc.client_id, Settings.ufc.client_secret,
+    :client_options => {
+      site: Settings.ufc.oauth_site
+    }
+end
