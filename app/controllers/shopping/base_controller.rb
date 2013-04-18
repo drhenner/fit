d8b449456b2662f13faa902d72812bebcd212f21
David Henner <drhenner@yahoo.com>
2013-04-18 09:08:38 -0700

    force non-logged in users to login

--- app/controllers/shopping/base_controller.rb
+++ app/controllers/shopping/base_controller.rb
@@ -14,7 +14,10 @@ class Shopping::BaseController < ApplicationController
 
 
   def ensure_in_united_states
-    if !current_user || current_user.country_id != Country::USA_ID
+    if !current_user
+      session[:return_to] = shopping_orders_url
+      redirect_to login_url
+    elsif current_user.country_id != Country::USA_ID
       flash[:alert] = 'Sorry, UFCFIT is only processing orders for customers in the United States.'
       redirect_to preorders_url
     end
