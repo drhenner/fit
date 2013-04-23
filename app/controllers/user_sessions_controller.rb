7b88af6ef5baaaa3bec30b2ffe93624aa6dbdda1
Khurram Zaman <khurram.zaman@gmail.com>
2013-04-23 18:03:30 +0500

    if oauth site logout url is defined then logout there.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -29,6 +29,7 @@ class UserSessionsController < ApplicationController
     current_user_session.destroy
     reset_session
     cookies.delete(:hadean_uid)
+    redirect_to "#{Settings.ufc.oauth_site_logout_url}" and return if Settings.ufc.oauth_site_logout_url
     redirect_to login_url, :notice => I18n.t('logout_successful')
   end
 
